9dd3c4fb2842bf38173a5bbb2e4b4a8cee8032b4
Alexandr Titarenko <nook17n1@gmail.com>
2026-02-27 18:41:23 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,61 @@
+from __future__ import annotations
+from __future__ import division
+from typing import Union
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: Union[int, float]) -> None:
+        self.km = km
+
+    def __add__(self, other: Union[Distance, int]) -> None:
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        return Distance(self.km + other)
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __lt__(self, other: Union[Distance, int]) -> None:
+        if isinstance(other, Distance):
+            return self.km < other.km
+        return self.km < other
+
+    def __gt__(self, other: Union[Distance, int]) -> None:
+        if isinstance(other, Distance):
+            return self.km > other.km
+        return self.km > other
+
+    def __eq__(self, other: Union[Distance, int]) -> None:
+        if isinstance(other, Distance):
+            return self.km == other.km
+        return self.km == other
+
+    def __le__(self, other: Union[Distance, int]) -> None:
+        if isinstance(other, Distance):
+            return self.km <= other.km
+        return self.km <= other
+
+    def __ge__(self, other: Union[Distance, int]) -> None:
+        if isinstance(other, Distance):
+            return self.km >= other.km
+        return self.km >= other
+
+    def __iadd__(self, other: Union[Distance, int, float]) -> None:
+        if isinstance(other, Distance):
+            self.km += other.km
+        elif isinstance(other, (int, float)):
+            self.km += other
+        return self
+
+    def __mul__(self, other: Union[Distance, int]) -> None:
+        if isinstance(other, Distance):
+            raise TypeError("Cannot multiply instance to instance")
+        return Distance(self.km * other)
+
+    def __truediv__(self, other: Union[Distance, int]) -> None:
+        if isinstance(other, Distance):
+            raise TypeError("Cannot divide instance to instance")
+        return Distance(round(self.km / other, 2))
